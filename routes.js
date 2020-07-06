780f03cf3249aae876ad7d64830333232c516ace
matheusgir <girardi.matheus@hotmail.com>
2020-07-06 18:19:36 -0300

    add routes and data.json

---
+++ routes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const routes = express.Router()
+const admin = require('../controllers/admin.js')
+const users = require('../controllers/users.js')
+
+routes.get("/", users.index);
+routes.get("/about", users.about);
+routes.get("/recipes", users.recipes);
+routes.get("/recipes/:index", users.show);
+
+routes.get("/admin/recipes", recipes.index); // Mostrar a lista de receitas
+routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
+routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
+routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
+routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
+routes.put("/admin/recipes", recipes.put); // Editar uma receita
+routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
